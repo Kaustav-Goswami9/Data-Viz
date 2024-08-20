45782152d2896f250a13ede549564fd7a6c3584b
Kaustav Goswami <75466816+Kaustav-Goswami9@users.noreply.github.com>
2024-08-20 20:16:37 +0530

    code updated.

--- topic_6.py
+++ topic_6.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
+# Load the data
 data = pd.read_csv('no_encoding_data.csv')
 
-# Selecting the relevant columns (Q12_2 to Q12_10)
-q2_columns = ['Q2_2', 'Q2_3', 'Q2_4', 'Q2_5',
-              'Q2_6', 'Q2_7', 'Q2_8', 'Q2_9', 'Q2_10']
+# Create a pivot table with 'Q18' as the index and 'voter_category' as columns
+pivot_table = data.pivot_table(index='Q18', columns='voter_category',
+                               aggfunc='size', fill_value=0)
 
-# Creating a pivot table to count the occurrences of each option in each question
-pivot_table = data.groupby('Q18')['voter_category'].value_counts()
-
+# Print the pivoted table
 print(pivot_table)
