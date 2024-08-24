612668e005d0a62c14d908d34b16f29f3c208395
Kaustav Goswami <75466816+Kaustav-Goswami9@users.noreply.github.com>
2024-08-24 17:25:28 +0530

    Updated topic 6.

--- topic_6.py
+++ topic_6.py
@@ -4,7 +4,7 @@ import pandas as pd
 data = pd.read_csv('no_encoding_data.csv')
 
 # Create a pivot table with 'Q18' as the index and 'voter_category' as columns
-pivot_table = data.pivot_table(index='Q18', columns='voter_category',
+pivot_table = data.pivot_table(index='Q21', columns='Q24',
                                aggfunc='size', fill_value=0)
 
 # Print the pivoted table
